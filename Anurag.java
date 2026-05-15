59590a0c6fdd5862159410617e4ac757d81adb07
Aman Tiwari <amantiwari@gmail.com>
2026-05-15 15:48:47 +0530

    login

--- Anurag.java
+++ Anurag.java
@@ -2,12 +2,20 @@ import java.util.Scanner;
 public class Anurag {
     public static void main(String[] args){
         Scanner  sc=new Scanner(System.in);
-        int num= sc.nextInt();
-        if(num>50&&num%2==0){
-            System.out.println("Special number");
-        }else {
-            System.out.println("normal number");
-        }
+//        int num= sc.nextInt();
+//        if(num>50&&num%2==0){
+//            System.out.println("Special number");
+//        }else {
+//            System.out.println("normal number");
+//        }
+     String username=sc.nextLine();
+     String password= sc.nextLine();
+     if(username.equals("admin")&&password.equals("1234")){
+         System.out.println("login succesful");
+     }else{
+         System.out.println("login failed");
+
+     }
 
     }
 }
