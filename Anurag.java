aee1272776f1a3243629df0ee42b788e1773579e
Aman Tiwari <amantiwari@gmail.com>
2026-05-15 15:55:42 +0530

    marks

--- Anurag.java
+++ Anurag.java
@@ -8,14 +8,23 @@ public class Anurag {
 //        }else {
 //            System.out.println("normal number");
 //        }
-     String username=sc.nextLine();
-     String password= sc.nextLine();
-     if(username.equals("admin")&&password.equals("1234")){
-         System.out.println("login succesful");
-     }else{
-         System.out.println("login failed");
+//     String username=sc.nextLine();
+//     String password= sc.nextLine();
+//     if(username.equals("admin")&&password.equals("1234")){
+//         System.out.println("login succesful");
+//     }else{
+//         System.out.println("login failed");
+//
+//     }
+    int marks=sc.nextInt();
+    if (marks>75){
+        System.out.println("Distinction");
+    } else if (marks>33) {
+        System.out.println("pass");
 
-     }
+    }else {
+        System.out.println("fail");
+    }
 
     }
 }
